0678e0e21d30bf9eec73f5e1814921cafdf0f161
Javad Aghababaei <javadbabaei009@gmail.com>
2022-07-11 12:42:44 +0430

    add middleware

---
+++ src/pages/_middleware.ts
@@ -0,0 +1,17 @@
+import type { NextFetchEvent, NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+export function middleware(req: NextRequest, ev: NextFetchEvent) {
+  const host = req.headers.get("host");
+  const subDomain = host.split(".")[0];
+
+  console.log(subDomain, "sub");
+  console.log(req.nextUrl.pathname);
+
+//   this condition for prod mode ! not dev mode
+  if (subDomain !== "sepehrcc") {
+    return NextResponse.redirect("/notfound", 301);
+  }
+
+  return NextResponse.next();
+}
